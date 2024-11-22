5dbb6aadfd3ff5cc7ca522d4f82f565738d1d140
sanghavi2431 <sanghavi2431@gmail.com>
2024-11-22 15:21:41 +0530

    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 set -e
-PIDS=$(ps -aef | grep "node /home/ubuntu/Woloo-Admin*/bin/app.js" | grep -v grep | awk '{print $2}')
-if [ -n "$PIDS" ]; then
-    for PID in $PIDS; do
-        sudo kill -9 $PID
-        echo "Killed Node server with PID $PID"
-    done
-else
-    echo "No Node server processes found."
-fi
 cd /home/ubuntu/Woloo-Admin-1
 echo "NPM install Started"
 npm install
